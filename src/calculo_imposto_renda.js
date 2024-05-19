15dde23a7ac43fa489e0b1a30ecdd7c01378724d
Deyse Aiala <deyseaiala23@gmail.com>
2024-05-19 00:16:06 -0300

    feat criando arquivo e funçao de desconto IR

---
+++ src/calculo_imposto_renda.js
@@ -0,0 +1,24 @@
+function calcularImpostoRenda(salarioBruto){
+
+    let descontoImposto = 0;
+
+    if(salarioBruto <= 2112){
+        descontoImposto = 0;
+    }
+    else if(salarioBruto <= 2826.65){
+        descontoImposto = salarioBruto * 0.075;
+    }
+    else if(salarioBruto <= 3751.05){
+        descontoImposto = salarioBruto * 0.15;
+    }
+    else if(salarioBruto <= 4664.68){
+        descontoImposto = salarioBruto * 0.225;
+    }
+    else{
+        descontoImposto = salarioBruto * 0.275;
+    }
+
+    return descontoImposto;
+}
+
+module.exports = calcularImpostoRenda;
--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-const calcularInss = require("./calculo_inss")
+const calcularImpostoRenda = require("./calculo_imposto_renda")
+//const calcularInss = require("./calculo_inss")
+
+
 
